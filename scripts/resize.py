dcf13660c11f25a1192ad86b31a972ea0d072371
Lowik Chanussot <lowikchanussot@gmail.com>
2017-08-03 22:09:36 +0200

    Add a script to resize images
    Also studied best combination for downsampling/upsampling the mask
    (best seems to downsample with PIL.BILINEAR, and upsample with
    cv2.CUBIC).

--- scripts/resize.py
+++ scripts/resize.py
@@ -1,17 +1,26 @@
+import argparse
 from PIL import Image
 import os
 import multiprocessing
 from functools import partial
 import logging
 import progressbar
+import numpy as np
+import cv2
 from utils_file import iter_files_with_ext
 
 
 def resize_worker(src_filename, dst_dir, target_size, is_boolean=False):
     try:
         img = Image.open(src_filename)
-        resample_method = Image.NEAREST if is_boolean else Image.BICUBIC
-        img_resized = img.resize(target_size, resample=resample_method)
+        if is_boolean:
+            img = Image.fromarray(np.array(img) * 255)
+            img_resized = img.resize(target_size, resample=Image.BILINEAR)
+            img_resized = img_resized > 127
+        else:
+            img = np.array(img)
+            img_resized = cv2.resize(img, target_size, interpolation=cv2.INTER_AREA)
+            img_resized = Image.fromarray(img_resized)
 
         src_basename = os.path.basename(src_filename)
         dst_basename, _ = os.path.splitext(src_basename)
@@ -23,17 +32,37 @@ def resize_worker(src_filename, dst_dir, target_size, is_boolean=False):
 
 def resize_all(src_dir, src_ext, dst_dir, target_size, is_boolean=False):
     os.makedirs(dst_dir, exist_ok=True)
-    iter_images = iter_files_with_ext(src_dir, ext=src_ext)
+    list_images = list(iter_files_with_ext(src_dir, ext=src_ext))
 
     resize_worker_spec = partial(resize_worker, dst_dir=dst_dir,
                                  target_size=target_size, is_boolean=is_boolean)
     with multiprocessing.Pool() as pool,\
-            progressbar.ProgressBar(max_value=progressbar.UnknownLength) as pbar:
+            progressbar.ProgressBar(0, len(list_images)) as pbar:
         for i, _ in enumerate(
-                pool.imap_unordered(resize_worker_spec, iter_images)):
+                pool.imap_unordered(resize_worker_spec, list_images)):
             pbar.update(i + 1)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    resize_all("../data/train/", ".jpg", "../data/train_240x160/", (240, 160))
+    parser = argparse.ArgumentParser(
+        description='Create a submission given a trained model.')
+    parser.add_argument('--src_dir',
+                        type=str, default="../data/train",
+                        help='Directory containing the images.')
+    parser.add_argument('--src_ext',
+                        type=str, default=".jpg",
+                        help='Extensions of images files in the src dir.')
+    parser.add_argument('--dst_dir',
+                        type=str, default="../data/train_240x160",
+                        help='Directory to save the resized images.')
+    parser.add_argument('--target_width',
+                        type=int, default=240,
+                        help="Target width")
+    parser.add_argument('--target_height',
+                        type=int, default=160,
+                        help="Target height")
+    args = parser.parse_args()
+
+    resize_all(args.src_dir, args.src_ext, args.dst_dir,
+               (args.target_width, args.target_height))
